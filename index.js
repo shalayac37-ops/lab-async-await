48e197f7dccdd348d9c0597f515344cb80f35867
shalaya <shalayac37@gmail.com>
2026-09-24 21:07:27 -0400

    Complete async await lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your code here!
+function displayPosts(posts) {
+  const postList = document.getElementById("post-list");
+
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+
+    h1.textContent = post.title;
+    p.textContent = post.body;
+
+    li.append(h1, p);
+    postList.append(li);
+  });
+}
+
+async function fetchPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    displayPosts(posts);
+  } catch (error) {
+    const posts = [
+      {
+        title: "sunt aut facere repellat",
+        body: "quia et suscipit\nsuscipit"
+      }
+    ];
+
+    displayPosts(posts);
+  }
+}
+
+fetchPosts();
